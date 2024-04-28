9b385f2a180cdc76ead8e4d0c41a36eee4babdba
MollyColvin101 <146596821+MollyColvin101@users.noreply.github.com>
2024-04-28 18:17:46 -0400

    custom commands definitions

--- cypress/support/customCommands.js
+++ cypress/support/customCommands.js
@@ -1,6 +1,5 @@
-Cypress.Commands.add('loginWithCredentials', (email, username, password) => {
+Cypress.Commands.add('loginWithCredentials', (username, password) => {
     cy.visit('https://www.saucedemo.com/'); 
-    cy.get('input[name="email"]').type(email);
     cy.get('input[name="username"]').type(username);
     cy.get('input[name="password"]').type(password);
     cy.get('form').submit(); 
